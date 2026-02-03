9465aa12d604eef79b51767d30a2293d82dff04a
ChibuezeSamObisike <samobisike@gmail.com>
2026-02-03 07:22:47 +0100

    chore: add image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const HERO_IMAGE =
-  "https://images.unsplash.com/photo-1500387467466-730600994c44?w=1200&q=80";
+  "https://images.unsplash.com/photo-1634757439914-23b8acb9d411?q=80&w=1588&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 export function Hero() {
   return (
